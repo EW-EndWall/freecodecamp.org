70b5ea99d6079f5a629c2cf152be056f11a6f322
EndWall <43109779+EW-EndWall@users.noreply.github.com>
2024-01-02 20:45:19 +0300

    update

---
+++ JavaScript-Algorithms-and-Data-Structures/Basic-Javascript/80.js
@@ -0,0 +1,15 @@
+function isLess(a, b) {
+  // Only change code below this line
+
+  switch (a < b) {
+    case true:
+      return true;
+      break;
+    case false:
+      return false;
+      break;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15);
